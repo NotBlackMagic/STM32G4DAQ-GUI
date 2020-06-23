480dded1d57c8eea6d6afa324d46af6457434578
Fabian Näf <fabian_naef@live.com>
2020-06-23 16:43:17 +0100

    -Work on Serial Handling and GUI/Graph updates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -47,6 +47,9 @@ namespace STM32G4DAQ {
 		float rxUSBDroppedRate = 0;
 		byte[] txBuffer = new byte[34];
 
+		int[] analogInADataIndex = new int[4];
+		double[][] analogInAData = new double[4][];
+
 		bool enableValueChangedEvents = false;
 
 		public SeriesCollection SeriesAnalogIn { get; set; }
@@ -54,6 +57,22 @@ namespace STM32G4DAQ {
 			enableValueChangedEvents = false;
 			InitializeComponent();
 			enableValueChangedEvents = true;
+
+			//Init Variables
+			analogInADataIndex[0] = 0;
+			analogInADataIndex[1] = 0;
+			analogInADataIndex[2] = 0;
+			analogInADataIndex[3] = 0;
+			analogInAData[0] = new double[1024];
+			analogInAData[1] = new double[1024];
+			analogInAData[2] = new double[1024];
+			analogInAData[3] = new double[1024];
+
+			//Init GUI Update dispatcher
+			DispatcherTimer dispatcherTimer = new DispatcherTimer();
+			dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000);
+			dispatcherTimer.Tick += GUIUpdateHandler;
+			dispatcherTimer.Start();
 
 			rxUSBStopwatch.Start();
 
@@ -92,26 +111,36 @@ namespace STM32G4DAQ {
 			DataContext = this;
 		}
 
-		private delegate void SerialReceiveDelegate(int opcode, byte[] rxData, int length);
+		private void GUIUpdateHandler(object sender, EventArgs e) {
+			rxUSBDatarateLabel.Content = rxUSBDatarate.ToString("0.0");
+			txUSBDatarateLabel.Content = txUSBDatarate.ToString("0.0");
+			rxUSBDroppedLabel.Content = rxUSBDroppedRate.ToString("0.0");
+
+			//SeriesAnalogIn[0].Values = new ChartValues<double>(analogInAData[0]);
+			//SeriesAnalogIn[1].Values = new ChartValues<double>(analogInAData[1]);
+			//SeriesAnalogIn[2].Values = new ChartValues<double>(analogInAData[2]);
+			//SeriesAnalogIn[3].Values = new ChartValues<double>(analogInAData[3]);
+		}
+
+		private delegate void SerialReceiveDelegate(byte[] rxData, int length);
 
 		private void OnSerialReceive(object sender, SerialDataReceivedEventArgs e) {
-			byte[] rxData = new byte[1024];
-			int opcode = serialPort.ReadByte();
-			int rxLength = (serialPort.ReadByte() << 8) + serialPort.ReadByte();
-			rxLength += 2;  //Add bytes CRC to length
+			byte[] rxData = new byte[2048];
+			int rxLength = serialPort.BytesToRead;
 
-			if (rxLength > 1024 || serialPort.BytesToRead > (serialPort.ReadBufferSize * 0.9)) {
+			//Discard if to many bytes to read
+			if (rxLength > 2048 || serialPort.BytesToRead > (serialPort.ReadBufferSize * 0.9)) {
 				rxDroppedCount += serialPort.BytesToRead;
 				serialPort.ReadExisting();
 			}
 			else {
 				serialPort.Read(rxData, 0, rxLength);
 
-				if(rxLength > 3) {
-					Dispatcher.Invoke(DispatcherPriority.Send, new SerialReceiveDelegate(SerialReceiveProcessing), opcode, rxData, rxLength);
+				if (rxLength > 5) {
+					Dispatcher.Invoke(DispatcherPriority.Send, new SerialReceiveDelegate(SerialReceiveProcessing), rxData, rxLength);
 				}
 
-				rxCount += rxLength + 3;
+				rxCount += rxLength;
 
 				if (rxUSBStopwatch.ElapsedMilliseconds > 1000) {
 					rxUSBStopwatch.Reset();
@@ -148,33 +177,46 @@ namespace STM32G4DAQ {
 			}
 		}
 
-		private void SerialReceiveProcessing(int opcode, byte[] rxData, int length) {
-			rxUSBDatarateLabel.Content = rxUSBDatarate.ToString("0.0");
-			txUSBDatarateLabel.Content = txUSBDatarate.ToString("0.0");
-			rxUSBDroppedLabel.Content = rxUSBDroppedRate.ToString("0.0");
+		private void SerialReceiveProcessing(byte[] rxData, int length) {
+			int index = 0;
+			while (index < length) {
+				int opcode = rxData[index++];
+				int packetLength = (rxData[index++] << 8) + rxData[index++];
+				int crc = (rxData[index + packetLength] << 8) + rxData[index + packetLength];
 
-			int crc = (rxData[length - 2] << 8) + rxData[length - 1];
+				switch (opcode) {
+					case Opcodes.txAnalogInA: {
+						int channel = rxData[index++];
 
-			length -= 2;    //Remove CRC length
-			switch (opcode) {
-				case Opcodes.txAnalogInA: {
-					int channel = rxData[0];
+						for(int i = 1; i < packetLength; i += 2) {
+							double value = (rxData[index++] << 8) + rxData[index++];
+							value = value * 2.048 / 4095 - 1.024;
+							value = value * 8 * 2;
 
-					for (int i = 0; i < (length / 2); i++) {
-						double value = ((rxData[1 + (2 * i)] << 8) + rxData[1 + (2 * i + 1)]) * 2.048 / 4095 - 1.024;
-						value = value * 16 * 2;
-						SeriesAnalogIn[channel - 1].Values.Add(value);
-						if (SeriesAnalogIn[channel - 1].Values.Count > 1000) {
-							SeriesAnalogIn[channel - 1].Values.RemoveAt(0);
+							//analogInAData[channel - 1][analogInADataIndex[channel - 1]++] = value;
+							//if (analogInADataIndex[channel - 1] >= 1024) {
+							//	analogInADataIndex[channel - 1] = 0;
+							//}
+
+							SeriesAnalogIn[channel - 1].Values.Add(value);
+							if (SeriesAnalogIn[channel - 1].Values.Count > 1024) {
+								SeriesAnalogIn[channel - 1].Values.RemoveAt(0);
+							}
 						}
+						break;
 					}
-					break;
+					default: {
+						index += packetLength;
+						break;
+					}
 				}
+
+				//Add CRC length, read already
+				index += 2;
 			}
 		}
 
 		private void SerialButtonClick(object sender, RoutedEventArgs e) {
-			//if (SerialButton.Content.ToString() == "Connect") {
 			if (serialPort.IsOpen == false) {
 				try {
 					serialPort.PortName = portNamesComboBox.Text;
@@ -182,24 +224,19 @@ namespace STM32G4DAQ {
 					serialPort.DataBits = 8;
 					serialPort.StopBits = StopBits.One;
 					serialPort.Parity = Parity.None;
-					//serialPort.DtrEnable = true;
 					serialPort.Open();
 
 					serialPort.DataReceived += new SerialDataReceivedEventHandler(OnSerialReceive);
-
-					//LogTextBox.Text += "INFO: Serial Connected: " + serialPort.PortName.ToString() + " : " + serialPort.BaudRate.ToString() + Environment.NewLine;
 
 					serialButton.Content = "Disconnect";
 					serialButton.Background = Brushes.LimeGreen;
 				}
 				catch (Exception ex) {
-					//LogTextBox.Text += "ERROR: " + ex.ToString() + Environment.NewLine;
+
 				}
 			}
 			else {
 				serialPort.Close();
-
-				//LogTextBox.Text += "INFO: Serial Disconnected" + Environment.NewLine;
 
 				serialButton.Content = "Connect";
 				serialButton.Background = Brushes.Red;
@@ -420,6 +457,7 @@ namespace STM32G4DAQ {
 
 			int offset = 0;
 			int.TryParse(analogOutAChannel1Offset.Text, out offset);
+			offset += 15000;
 			float step = 30000 / 4096;
 			int nSteps = Convert.ToInt32(offset / step);
 
@@ -445,7 +483,7 @@ namespace STM32G4DAQ {
 			data[8] = (byte)amp;
 			data[9] = (byte)dc;
 
-			//SerialSendCommand(Opcodes.setAnalogOutACH, data);
+			SerialSendCommand(Opcodes.setAnalogOutACH, data);
 		}
 	}
 }
